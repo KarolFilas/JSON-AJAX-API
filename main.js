f20dafcc37337aa6cf5f24f0f57357a3b47171a6
Karol Filas <filaskarol@gmail.com>
2023-03-10 11:12:40 +0100

    promise

--- main.js
+++ main.js
@@ -4,14 +4,42 @@ const img = document.querySelector('img')
 const URL = 'https://dog.ceo/api/breeds/image/random'
 
 // fetch(URL).then().catch()
-
 // fetch(URL).then(res => console.log(res)).catch(err => console.err(err))
-
 // fetch(URL).then(res => res.json()).then(data => img.setAttribute('src', data.message)).catch(err => console.error(err))
 
 btn.addEventListener('click', () => {
     fetch(URL).then(res => res.json()).then(data => img.setAttribute('src', data.message)).catch(err => console.error(err))
 })
+
+console.log('---------promise-------');
+
+const test = new Promise((resolve, reject) => {
+    if (true) {
+        resolve('jest ok')
+    } else {
+        reject('błąd')
+    }
+})
+
+test.then(info => console.log(info)).catch(err => console.error(err))
+
+console.log('---------Async & await-------');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
