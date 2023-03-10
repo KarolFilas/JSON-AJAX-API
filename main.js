b7eb52ab372cd35ffd4b86dfe132119757f73ca0
Karol Filas <filaskarol@gmail.com>
2023-03-10 12:05:29 +0100

    Async & await

--- main.js
+++ main.js
@@ -25,19 +25,60 @@ test.then(info => console.log(info)).catch(err => console.error(err))
 
 console.log('---------Async & await-------');
 
+const checkAge = age => {
+    return new Promise((resolve, reject) => {
+        if (age >= 18) {
+            resolve()
+        } else {
+            reject('Masz za mało lat!')
+        }
+    })
+}
 
+const doubleCheck = () => {
+    return new Promise(resolve => {
+        console.log('sprawdzam jeszcze raz...')
+        setTimeout(() => {
+            resolve('Faktycznie, wiek się zgadza.')
+        }, 1000);
+    })
+}
 
+// checkAge(50).then(() => {
+//     console.log('Chyba możesz wejsc')
+//     return doubleCheck()
+// }).then(res => console.log(res)).then(() => {
+//     console.log('weryfikacja zakończonona')
+// }).catch(error => console.error(error))
 
+async function one() {
+    try {
+        await checkAge(22)
+        console.log('chyba możesz wejsc')
+        await doubleCheck()
+        console.log('Faktycznie, wiek się zgadza.')
+        console.log('weryfikacja zakończonona')
+    } catch {
+        console.error('błąd, masz za mało lat');
+    }
 
+}
 
+one()
 
+fetch(URL).then(res => res.json()).then(data => console.log(data)).catch(error => console.error(error))
 
+async function urlTest() {
+    try {
+        const res = await fetch(URL)
+        const data = await res.json()
+        console.log(data);
+    } catch {
+        console.error('błąd, errors');
+    }
+}
 
-
-
-
-
-
+urlTest()
 
 
 
